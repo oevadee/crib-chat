e0776de26ff01f80b38e33b20a1e2cea5b4e9628
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-02-13 03:23:03 +0100

    finished working on NavBar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,8 @@ import "./Navbar.scss";
 import { GiBottledShadow } from "react-icons/gi";
 import { BiSearchAlt } from "react-icons/bi";
 import { AiOutlineMessage, AiOutlineBell } from "react-icons/ai";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import { IconContext } from "react-icons";
 
 export interface NavbarProps {
   onClick?: () => void;
@@ -11,24 +12,29 @@ export interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ ...props }) => {
   return (
-    <div className="navbar">
-      <div className="navbar__logo">
-        <GiBottledShadow size="2rem" color="#fff" />
+    <IconContext.Provider value={{ size: "1.5rem", color: "#a6a8ff" }}>
+      <div className="navbar">
+        <div className="navbar__logo">
+          <GiBottledShadow color="#fff" size="2rem" />
+        </div>
+        <nav>
+          <NavLink to="/">
+            <BiSearchAlt />
+          </NavLink>
+          <NavLink to="/">
+            <AiOutlineMessage />
+          </NavLink>
+        </nav>
+        <div className="navbar__account">
+          <AiOutlineBell />
+          <img
+            src="https://avatars.githubusercontent.com/u/62460030?s=460&u=1bea4d850a2fd895c6954f10196dfb0ab4cbbb3e&v=4"
+            alt="avatar"
+          />
+        </div>
       </div>
-      <nav>
-        <NavLink to="/">
-          <BiSearchAlt size="2rem" />
-        </NavLink>
-        <NavLink to="/">
-          <AiOutlineMessage size="2rem" />
-        </NavLink>
-      </nav>
-      <div className="navbar__account">
-        <AiOutlineBell size="2rem" />
-        <img src="https://avatars.githubusercontent.com/u/62460030?s=460&u=1bea4d850a2fd895c6954f10196dfb0ab4cbbb3e&v=4" alt="avatar"/>
-      </div>
-    </div>
+    </IconContext.Provider>
   );
 };
 
-export default Navbar
+export default Navbar;
