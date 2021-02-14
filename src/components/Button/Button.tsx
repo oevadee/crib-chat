9b29efe22dfe264712a6c7a1d4e102bc3e8c2259
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-02-14 13:20:52 +0100

    started working on Profile route

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,14 @@ import "./Button.scss";
 export interface ButtonProps {
   text: string;
   uiType: string;
+  bgColor: string;
+  color: string;
 }
 
-const Button: FC<ButtonProps> = ({ text, uiType }) => {
+const Button: FC<ButtonProps> = ({ text, uiType, bgColor, color }) => {
   return (
-    <button className={`button ${uiType}`}>
-      <p>{text}</p>
+    <button className={`button ${uiType} ${bgColor}`}>
+      <p style={{ color: color }}>{text}</p>
     </button>
   );
 };
--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Button from "../../components/Button/Button";
 import "./Profile.scss";
 
 interface ProfileProps {}
@@ -8,11 +9,24 @@ const Profile: FC<ProfileProps> = () => {
     <div className="profile">
       <div className="profile__settings">
         <h1>Settings</h1>
-        <div className="card">
+        <div className="profile__settings__card">
+          <img src="/img/abstract-bg2.svg" alt="" />
           <h3>Profile Informations</h3>
           <small>Complete your profile to unlock all features</small>
-          <button></button>
+          <Button
+            text="Complete Your Profile"
+            uiType="longSlim"
+            bgColor="white"
+            color="rgb(12, 99, 159)"
+          />
         </div>
+      </div>
+      <div className="profile__actions">
+        <div className="profile__actions__header">
+          <h2>Account Settings</h2>
+        </div>
+        <h3>Personal Informations</h3>
+        <small>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</small>
       </div>
     </div>
   );
